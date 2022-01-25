ae4816f2aea26439538c56ac16d91b0d31a655a8
anton.tretiak <anton.tretiak1@nure.ua>
2022-01-25 17:58:00 +0200

    try to

--- lib/screens/screen2.dart
+++ lib/screens/screen2.dart
@@ -33,75 +33,77 @@ class _Screen2State extends State<Screen2> {
       return _ui1Layer(context, widget.taskPerformer, null, filmViewModel);
     }
   }
-}
 
-Widget _ui1Layer(BuildContext context, TaskPerformer taskPerformer,
-    [FilmState? filmState, FilmViewModel? filmViewModel]) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('List of Films'),
-    ),
-    body: _ui2Layer(context, taskPerformer, filmState, filmViewModel),
-    floatingActionButton: FloatingActionButton(
-      child: const Icon(Icons.rotate_left),
-      tooltip: 'rotate',
-      onPressed: () {
-        Navigator.of(context)
-            .pushNamed(Screen4.detailsScreenRoute, arguments: taskPerformer);
-        SystemChrome.setPreferredOrientations([
-          DeviceOrientation.landscapeLeft,
-          DeviceOrientation.landscapeRight
-        ]);
-        if (taskPerformer == TaskPerformer.bloc) {
-          BlocProvider.of<FilmBloc>(context).add(SelectFilmEvent(const Film(
-            '',
-            '',
-          )));
-        } else {
-          filmViewModel?.setFilm(const Film('', ''));
-        }
-      },
-    ),
-  );
-}
+  Widget _ui1Layer(BuildContext context, TaskPerformer taskPerformer,
+      [FilmState? filmState, FilmViewModel? filmViewModel]) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List of Films'),
+      ),
+      body: _ui2Layer(context, taskPerformer, filmState, filmViewModel),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.rotate_left),
+        tooltip: 'rotate',
+        onPressed: () {
+          Navigator.of(context)
+              .pushNamed(Screen4.detailsScreenRoute, arguments: taskPerformer);
+          SystemChrome.setPreferredOrientations([
+            DeviceOrientation.landscapeLeft,
+            DeviceOrientation.landscapeRight
+          ]);
+          flButton2Screen();
+          /*if (taskPerformer == TaskPerformer.bloc) {
+            BlocProvider.of<FilmBloc>(context).add(SelectFilmEvent(const Film(
+              '',
+              '',
+            )));
+          } else {
+            filmViewModel?.setFilm(const Film('', ''));
+          }*/
+        },
+      ),
+    );
+  }
 
-Widget _ui2Layer(BuildContext context, TaskPerformer taskPerformer,
-    [FilmState? filmState, FilmViewModel? filmViewModel]) {
-  return RefreshIndicator(
-      onRefresh: () async {
-        if (taskPerformer == TaskPerformer.bloc) {
-          BlocProvider.of<FilmBloc>(context).add(ShuffleFilmEvent());
-        } else {
-          filmViewModel?.getPullToRefresh();
-        }
-      },
-      child: _ui3Layer(context, taskPerformer, filmState, filmViewModel));
-}
+  Widget _ui2Layer(BuildContext context, TaskPerformer taskPerformer,
+      [FilmState? filmState, FilmViewModel? filmViewModel]) {
+    return RefreshIndicator(
+        onRefresh: () async {
+          if (taskPerformer == TaskPerformer.bloc) {
+            BlocProvider.of<FilmBloc>(context).add(ShuffleFilmEvent());
+          } else {
+            filmViewModel?.getPullToRefresh();
+          }
+        },
+        child: _ui3Layer(context, taskPerformer, filmState, filmViewModel));
+  }
 
-Widget _ui3Layer(BuildContext context, TaskPerformer taskPerformer,
-    [FilmState? filmState, FilmViewModel? filmViewModel]) {
-  if (filmViewModel?.filmList.isNotEmpty ?? filmState != null) {
-    return ListView.builder(
-        itemCount: count(taskPerformer, filmState, filmViewModel),
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              onTap: () {
-                Navigator.of(context).pushNamed(Screen3.detailsScreenRoute,
-                    arguments: taskPerformer);
-                if (taskPerformer == TaskPerformer.bloc &&
-                    filmState is FilmLoadedState) {
-                  BlocProvider.of<FilmBloc>(context)
-                      .add(SelectFilmEvent(filmState.films[index]));
-                } else {
-                  filmViewModel?.getSelectedFilm(filmViewModel.filmList[index]);
-                }
-              },
-              title: title(taskPerformer, index, filmState, filmViewModel),
-            ),
-          );
-        });
-  } else {
-    return loading();
+  Widget _ui3Layer(BuildContext context, TaskPerformer taskPerformer,
+      [FilmState? filmState, FilmViewModel? filmViewModel]) {
+    if (filmViewModel?.filmList.isNotEmpty ?? filmState != null) {
+      return ListView.builder(
+          itemCount: count(taskPerformer, filmState, filmViewModel),
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).pushNamed(Screen3.detailsScreenRoute,
+                      arguments: taskPerformer);
+                  if (taskPerformer == TaskPerformer.bloc &&
+                      filmState is FilmLoadedState) {
+                    BlocProvider.of<FilmBloc>(context)
+                        .add(SelectFilmEvent(filmState.films[index]));
+                  } else {
+                    filmViewModel
+                        ?.getSelectedFilm(filmViewModel.filmList[index]);
+                  }
+                },
+                title: title(taskPerformer, index, filmState, filmViewModel),
+              ),
+            );
+          });
+    } else {
+      return loading();
+    }
   }
 }
--- lib/task_performer.dart
+++ lib/task_performer.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:start_project/viewmodel/film_view_model.dart';
+
+import 'bloc/bloc.dart';
+import 'film.dart';
+
 enum TaskPerformer { bloc, mvvm }
+
+Perfoming(TaskPerformer taskPerformer, BuildContext context,
+    FilmState filmState, FilmViewModel filmViewModel) {
+  void flButton2Screen(taskPerformer) {
+    if (taskPerformer == TaskPerformer.bloc) {
+      BlocProvider.of<FilmBloc>(context).add(SelectFilmEvent(const Film(
+        '',
+        '',
+      )));
+    } else {
+      filmViewModel.setFilm(const Film('', ''));
+    }
+  }
+}
