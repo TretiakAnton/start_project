507d638c4b67d9bbddcc6179ec608c9b8c8927a3
anton.tretiak <anton.tretiak1@nure.ua>
2022-02-01 15:20:37 +0200

    /

--- lib/repo/films_repo.dart
+++ lib/repo/films_repo.dart
@@ -7,11 +7,8 @@ Uri address = Uri.parse('https://putsreq.com/1vupmovCKZ78Jzs8qeCm');
 
 class FilmRepository {
   Future<List<Film>> getFilms() async {
-    print('start');
     final http.Response response = await http.get(address);
-    print('process');
     final List<dynamic> films = jsonDecode(response.body);
-    print('end');
     return films.map((json) => Film.fromJSON(json)).toList();
   }
 }
